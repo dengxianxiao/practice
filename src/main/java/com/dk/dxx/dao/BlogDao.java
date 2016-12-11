45213958a1ab9b22b3f7b98f0ce82497255d5981
dengxianxiao <784373119@qq.com>
2016-12-11 22:47:09 +0800

    创建bloggerDao并创建相应mapper

--- src/main/java/com/dk/dxx/dao/BlogDao.java
+++ src/main/java/com/dk/dxx/dao/BlogDao.java
@@ -1,5 +1,12 @@
 package com.dk.dxx.dao;
 
+/**
+ * 博客信息操作接口
+ * 
+ * @author dxx
+ *
+ */
 public class BlogDao {
 
+	
 }
--- src/main/java/com/dk/dxx/service/impl/BookServiceImpl.java
+++ src/main/java/com/dk/dxx/service/impl/BookServiceImpl.java
@@ -15,12 +15,12 @@ import com.dk.dxx.entity.Book;
 import com.dk.dxx.enums.AppointStateEnum;
 import com.dk.dxx.service.BookService;
 
-@Service
+//@Service
 public class BookServiceImpl implements BookService {
 	
 	private Logger logger = Logger.getLogger(this.getClass());
 
-	//注入service依赖
+	//娉ㄥ叆service渚濊禆
 	@Autowired
 	private BookDao bookDao;
 	
@@ -36,34 +36,34 @@ public class BookServiceImpl implements BookService {
 		return bookDao.queryAll(0, 100);
 	}
 
-	@Transactional
+//	@Transactional
 	/**
-     * 使用注解控制事务方法的优点： 1.开发团队达成一致约定，明确标注事务方法的编程风格
-     * 2.保证事务方法的执行时间尽可能短，不要穿插其他网络操作，RPC/HTTP请求或者剥离到事务方法外部
-     * 3.不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制
+     * 浣跨敤娉ㄨВ鎺у埗浜嬪姟鏂规硶鐨勪紭鐐癸細 1.寮�彂鍥㈤槦杈炬垚涓�嚧绾﹀畾锛屾槑纭爣娉ㄤ簨鍔℃柟娉曠殑缂栫▼椋庢牸
+     * 2.淇濊瘉浜嬪姟鏂规硶鐨勬墽琛屾椂闂村敖鍙兘鐭紝涓嶈绌挎彃鍏朵粬缃戠粶鎿嶄綔锛孯PC/HTTP璇锋眰鎴栬�鍓ョ鍒颁簨鍔℃柟娉曞閮�
+     * 3.涓嶆槸鎵�湁鐨勬柟娉曢兘闇�浜嬪姟锛屽鍙湁涓�潯淇敼鎿嶄綔锛屽彧璇绘搷浣滀笉闇�浜嬪姟鎺у埗
      */
 	public AppointExecution appointBook(Long bookId, Long studentId) {
 		try {
-			//减库存
+			//鍑忓簱瀛�
 			int update = bookDao.reduceNumber(bookId);
 			if (update <= 0) {
-				//库存不足
+				//搴撳瓨涓嶈冻
 				return new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
 			} else {
-				//执行预约操作
+				//鎵ц棰勭害鎿嶄綔
 				int insert = appointmentDao.insertAppointment(bookId, studentId);
 				if (insert <= 0) {
-					//重复预约
+					//閲嶅棰勭害
 					return new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
 				} else {
-					//预约成功
+					//棰勭害鎴愬姛
 					Appointment appointment = appointmentDao.queryByKeyWithBook(bookId, studentId);
 					return new AppointExecution(bookId, AppointStateEnum.SUCCESS, appointment); 
 				}
 			}
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
-			// 所有编译期异常转换为运行期异常
+			// 鎵�湁缂栬瘧鏈熷紓甯歌浆鎹负杩愯鏈熷紓甯�
 			return new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
 		}
 	}
